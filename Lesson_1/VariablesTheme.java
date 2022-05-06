fafa9b7bfa3a035149ac719f2f08c21643f5711d
a.markovkin <markovkin88@gmail.com>
2022-05-06 12:39:49 +0300

    4. Вносит исправления в домашнее задание по теме - переменные

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -1,7 +1,6 @@
 public class VariablesTheme {
     public static void main(String[] args) {
-
-        System.out.println("1. Создание переменных и вывод их значений на консоль");
+        System.out.println("1. РЎРѕР·РґР°РЅРёРµ РїРµСЂРµРјРµРЅРЅС‹С… Рё РІС‹РІРѕРґ РёС… Р·РЅР°С‡РµРЅРёР№ РЅР° РєРѕРЅСЃРѕР»СЊ");
         byte cpuCoreNumbers = 6;
         short cpuThreadNumbers = 6;
         int ram = 16;
@@ -20,7 +19,7 @@ public class VariablesTheme {
         System.out.println("ssdFreespace: " + ssdFreespace);
         System.out.println("hddFreespace: " + hddFreespace);
 
-        System.out.println("\n2. Расчет стоимости товара со скидкой");
+        System.out.println("\n2. Р Р°СЃС‡РµС‚ СЃС‚РѕРёРјРѕСЃС‚Рё С‚РѕРІР°СЂР° СЃРѕ СЃРєРёРґРєРѕР№");
         int priceX = 100;
         int priceY = 200;
         int totalPrice = priceX + priceY;
@@ -31,91 +30,95 @@ public class VariablesTheme {
         System.out.println("total discount: " + totalDiscount);
         System.out.println("total price with discount: " + totalDiscountedPrice);
 
-        System.out.println("\n3. Вывод на консоль слова JAVA");
+        System.out.println("\n3. Р’С‹РІРѕРґ РЅР° РєРѕРЅСЃРѕР»СЊ СЃР»РѕРІР° JAVA");
         System.out.println("   J    a  v     v  a   ");
         System.out.println("   J   a a  v   v  a a  ");
         System.out.println("J  J  aaaaa  V V  aaaaa ");
         System.out.println(" JJ  a     a  V  a     a");
 
-        System.out.println("\n4. Отображение min и max значений числовых типов данных");
+        System.out.println("\n4. РћС‚РѕР±СЂР°Р¶РµРЅРёРµ min Рё max Р·РЅР°С‡РµРЅРёР№ С‡РёСЃР»РѕРІС‹С… С‚РёРїРѕРІ РґР°РЅРЅС‹С…");
         byte maxByte = Byte.MAX_VALUE;
         short maxShort = Short.MAX_VALUE;
         int maxInt = Integer.MAX_VALUE;
         long maxLong = Long.MAX_VALUE;
         System.out.println("max byte: " + maxByte);
-        System.out.println("max byte incremented by 1: " + (maxByte += 1));
-        System.out.println("max byte decremented by 1: " + (maxByte -= 1));
+        System.out.println("max byte incremented by 1: " + ++maxByte);
+        System.out.println("max byte decremented by 1: " + --maxByte);
         System.out.println("max short: " + maxShort);
-        System.out.println("max short incremented by 1: " + (maxShort += 1));
-        System.out.println("max short decremented by 1: " + (maxShort -= 1));
+        System.out.println("max short incremented by 1: " + ++maxShort);
+        System.out.println("max short decremented by 1: " + --maxShort);
         System.out.println("max int: " + maxInt);
-        System.out.println("max int incremented by 1: " + (maxInt += 1));
-        System.out.println("max int decremented by 1: " + (maxInt -= 1));
+        System.out.println("max int incremented by 1: " + ++maxInt);
+        System.out.println("max int decremented by 1: " + --maxInt);
         System.out.println("max long: " + maxLong);
-        System.out.println("max long incremented by 1: " + (maxLong += 1));
-        System.out.println("max long decremented by 1: " + (maxLong -= 1));
+        System.out.println("max long incremented by 1: " + ++maxLong);
+        System.out.println("max long decremented by 1: " + --maxLong);
 
-        System.out.println("\n5. Перестановка значений переменных");
-        float decimalVarFirst = 10.2f;
-        float decimalVarSecond = 5.4f;
-        float bufferVar = 0.0f;
-        System.out.println("first decimal variable: " + decimalVarFirst);
-        System.out.println("second decimal variable: " + decimalVarSecond);
-        bufferVar = decimalVarFirst;
-        decimalVarFirst = decimalVarSecond;
-        decimalVarSecond = bufferVar;
-        System.out.println("new first decimal: " + decimalVarFirst);
-        System.out.println("new second decimal: " + decimalVarSecond);
+        System.out.println("\n5. РџРµСЂРµСЃС‚Р°РЅРѕРІРєР° Р·РЅР°С‡РµРЅРёР№ РїРµСЂРµРјРµРЅРЅС‹С…");
+        float num1 = 10.2f;
+        float num2 = 5.4f;
+        float buffer = 0.0f;
+        System.out.println("first decimal variable: " + num1);
+        System.out.println("second decimal variable: " + num2);
+        buffer = num1;
+        num1 = num2;
+        num2 = buffer;
+        System.out.println("new first decimal: " + num1);
+        System.out.println("new second decimal: " + num2);
 
-        System.out.println("\n6. Вывод символов и их кодов");
-        int numFirst = 35;
-        int numSecond = 38;
-        int numThird = 64;
-        int numFourth = 94;
-        int numFifth = 95;
-        System.out.println("num first: " + numFirst + ", ascii-code: " + (char)numFirst);
-        System.out.println("num second: " + numSecond + ", ascii-code: " + (char)numSecond);
-        System.out.println("num third: " + numThird + ", ascii-code: " + (char)numThird);
-        System.out.println("num fourth: " + numFourth + ", ascii-code: " + (char)numFourth);
-        System.out.println("num fifth: " + numFifth + ", ascii-code: " + (char)numFifth);
+        System.out.println("\n6. Р’С‹РІРѕРґ СЃРёРјРІРѕР»РѕРІ Рё РёС… РєРѕРґРѕРІ");
+        int numberCode = 35;
+        int ampersandCode = 38;
+        int atsignCode = 64;
+        int caretCode = 94;
+        int underlineCode = 95;
+        System.out.println("number symbol: " + numberCode + ", ascii-code: " + (char) numberCode);
+        System.out.println("ampersand: " + ampersandCode + ", ascii-code: " + 
+            (char) ampersandCode);
+        System.out.println("at sign: " + atsignCode + ", ascii-code: " + 
+            (char) atsignCode);
+        System.out.println("caret: " + caretCode + ", ascii-code: " + 
+            (char) caretCode);
+        System.out.println("underline: " + underlineCode + ", ascii-code: " + 
+            (char) underlineCode);
 
-        System.out.println("\n7. Произведение и сумма цифр числа");
-        int numThreeFourFive = 345;
-        int firstNum = numThreeFourFive / 100;
-        int secondNum = (numThreeFourFive - (firstNum * 100)) / 10;
-        int thirdNum = numThreeFourFive - ((firstNum * 100) + (secondNum * 10));
-        System.out.println("345 numbers multiplication: " + (firstNum * secondNum * thirdNum));
-        System.out.println("345 numbers summ: " + (firstNum + secondNum + thirdNum));
+        System.out.println("\n7. РџСЂРѕРёР·РІРµРґРµРЅРёРµ Рё СЃСѓРјРјР° С†РёС„СЂ С‡РёСЃР»Р°");
+        int srcNum = 345;
+        int firstDigit = srcNum / 100;
+        int secondDigit = (srcNum % 100) / 10;
+        int thirdDigit = (srcNum % 100) % 10;
+        System.out.println("345 digits multiplication: " + (firstDigit * secondDigit * thirdDigit));
+        System.out.println("345 digits summ: " + (firstDigit + secondDigit + thirdDigit));
 
-        System.out.println("\n8. Вывод на консоль ASCII-арт Дюка");
+        System.out.println("\n8. Р’С‹РІРѕРґ РЅР° РєРѕРЅСЃРѕР»СЊ ASCII-Р°СЂС‚ Р”СЋРєР°");
         char space = ' ';
-        char solidus = '/';
-        char reverseSolidus = '\\';
-        char lowLine = '_';
-        char leftParenthesis = '(';
-        char rightParenthesis = ')';
-        System.out.println("" + space + space + space + space + solidus + reverseSolidus + space + space + space + space + space);
-        System.out.println("" + space + space + space  + solidus + space + space + reverseSolidus + space + space + space + space);
-        System.out.println("" + space + space  + solidus + lowLine + leftParenthesis + space + rightParenthesis + reverseSolidus + space + space + space + space);
-        System.out.println("" + space + solidus + space + space + space + space + space+ space + reverseSolidus + space + space);
-        System.out.println("" + solidus + lowLine + lowLine + lowLine + lowLine + solidus + reverseSolidus + lowLine + lowLine + reverseSolidus + space);
+        char slash = '/';
+        char reverseSlash = '\\';
+        char underline = '_';
+        char leftBracket = '(';
+        char rightBracket = ')';
+        System.out.println("" + space + space + space + space + slash + reverseSlash + space + space + space + space + space);
+        System.out.println("" + space + space + space  + slash + space + space + reverseSlash + space + space + space + space);
+        System.out.println("" + space + space  + slash + underline + leftBracket + space + rightBracket + reverseSlash + space + space + space + space);
+        System.out.println("" + space + slash + space + space + space + space + space+ space + reverseSlash + space + space);
+        System.out.println("" + slash + underline + underline + underline + underline + slash + reverseSlash + underline + underline + reverseSlash + space);
 
-        System.out.println("\n9. Отображение количества сотен, десятков и единиц числа");
-        int numOneTwoThree = 123;
-        int numOfHundreds = numOneTwoThree / 100;
-        int numOfTens = (numOneTwoThree - (numOfHundreds * 100)) / 10;
-        int numOfOnes = numOneTwoThree - ((numOfHundreds * 100) + (numOfTens * 10));
+        System.out.println("\n9. РћС‚РѕР±СЂР°Р¶РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЃРѕС‚РµРЅ, РґРµСЃСЏС‚РєРѕРІ Рё РµРґРёРЅРёС† С‡РёСЃР»Р°");
+        int srcNum2 = 123;
+        int numOfHundreds = srcNum2 / 100;
+        int numOfTens = (srcNum2 % 100) / 10;
+        int numOfOnes = (srcNum2 % 100) % 10;;
         int valueOfHundreds = numOfHundreds * 100;
         int valueOfTens = numOfTens * 10;
         System.out.println("123 number of hundreds: " + valueOfHundreds);
         System.out.println("123 number of tens: " + valueOfTens);
         System.out.println("123 number of ones: " + numOfOnes);
 
-        System.out.println("\n10. Преобразование секунд");
-        int initialSeconds = 86399;
-        int hours = initialSeconds / 3600;
-        int minutes = (initialSeconds % 3600) / 60;
-        int seconds = (initialSeconds % 3600) % 60;
+        System.out.println("\n10. РџСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёРµ СЃРµРєСѓРЅРґ");
+        int totalSeconds = 86399;
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = (totalSeconds % 3600) % 60;
         System.out.println(hours + ":" + minutes + ":" + seconds);
     }
 }
