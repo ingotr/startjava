766b842e53c4e6f6d27525a9de49c648162d4d67
amarkovkin <markovkin88@gmail.com>
2022-05-12 22:25:54 +0300

    12. Добавляет домашнее задание 2-го урока по теме - класс

---
+++ Lesson_2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+    String gender = "male";
+    String name = "Иван Васильевич";
+    String job = "в творческом поиске";
+    float height = 1.84f;
+    int weight = 93;
+    int age = 60;
+
+    void walk() {
+        System.out.println("гуляет в парке");
+    }
+
+    String seat() {
+        return "sitting";
+    }
+
+    void run() {
+        System.out.println("пробегает 10 км и приходит в форму");
+        weight--;
+    }
+
+    void talk() {
+        System.out.println("Меняю профессию");;
+    }
+
+    boolean learnJava() {
+        return true;
+    }
+}
