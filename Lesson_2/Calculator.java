a827a0e664ee016be96d29d059d4367fceaa7c10
amarkovkin <markovkin88@gmail.com>
2022-05-14 23:08:50 +0300

    16. Переписывают условные операторы через switch. Убирает метод main. Добавляет метод calculate

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,40 @@
+public class Calculator {
+
+    public int calculate(int a, int b, char sign) {
+        int result = 0;
+        switch(sign) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        case '^':
+            result = 1;
+            int i = 0;
+
+            while(i < Math.abs(b)) {
+                result *= a;
+                i++;
+            }
+
+            if(b < 0) {
+                result = 1 / result;
+            }
+            break;
+        case '%':
+            result = a % b;
+            break;
+        default:
+            System.out.println("Неизвестный оператор. Повторите ввод");
+            System.out.println("Доступные операторы: +, -, *, /, ^, %");
+        }
+        return result;
+    }
+}
