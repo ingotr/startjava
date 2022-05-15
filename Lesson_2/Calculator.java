e8de36b659dd7eb6251caaf25f36c3bedc6e8445
amarkovkin <markovkin88@gmail.com>
2022-05-15 15:44:23 +0300

    20. Правит отступы в блоке switch

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -3,38 +3,38 @@ public class Calculator {
     public int calculate(int a, int b, char sign) {
         int result = 0;
         switch(sign) {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            result = a / b;
-            break;
-        case '^':
-            result = 1;
-            int i = 0;
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            case '^':
+                result = 1;
+                int i = 0;
 
-            while(i < Math.abs(b)) {
-                result *= a;
-                i++;
-            }
+                while(i < Math.abs(b)) {
+                    result *= a;
+                    i++;
+                }
 
-            if(b < 0) {
-                result = 1 / result;
+                if(b < 0) {
+                    result = 1 / result;
+                }
+                break;
+            case '%':
+                result = a % b;
+                break;
+            default:
+                System.out.println("Неизвестный оператор. Повторите ввод");
+                System.out.println("Доступные операторы: +, -, *, /, ^, %");
             }
-            break;
-        case '%':
-            result = a % b;
-            break;
-        default:
-            System.out.println("Неизвестный оператор. Повторите ввод");
-            System.out.println("Доступные операторы: +, -, *, /, ^, %");
-        }
         return result;
     }
 }
