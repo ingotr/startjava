cb2224979d55079d1e891725650fe97d567a5c53
amarkovkin <markovkin88@gmail.com>
2022-05-15 15:45:24 +0300

    21. Убирает лишнее условия, упрощает условия в цикле while

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,10 +4,10 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        String continueCalculate = "yes";
-        int result = 0;
-
+        String userAnswer = "yes";
+        
         do {
+            int result = 0;
             System.out.println("Введите первое число: ");
             int a = scanner.nextInt();
             System.out.println("Введите знак математической операции: ");
@@ -19,13 +19,9 @@ public class CalculatorTest {
             System.out.println(a + " " + sign + " " + b + " = " + result);
             
             do {
-                if(continueCalculate.equals("no")) {
-                    break;
-                }
-                
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
-                continueCalculate = scanner.next();
-            } while(!((continueCalculate.equals("no") || continueCalculate.equals("yes"))));
-        } while(!continueCalculate.equals("no"));
+                userAnswer = scanner.next();
+            } while(!userAnswer.equals("no") && !userAnswer.equals("yes"));
+        } while(!userAnswer.equals("no"));
     }
 }
