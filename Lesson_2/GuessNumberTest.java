b3d4d02ff24b2ae93321c4b81a6265d4158530f8
Markovkin Aleksandr <markovkin88@gmail.com>
2022-05-18 12:44:29 +0300

    33. Упрощает проверки в методах. Убирает лишние переменные.

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -5,30 +5,20 @@ public class GuessNumberTest {
         Scanner scanner = new Scanner(System.in);
         System.out.format("%nДобро пожаловать в игру - угадай число%n");
         System.out.println("Введите имя первого игрока: ");
-        String tmp = scanner.next();
-        Player playerOne = new Player(tmp);
+        Player playerOne = new Player(scanner.next());
         
         System.out.println("Введите имя второго игрока: ");
-        tmp = scanner.next();
-        Player playerTwo = new Player(tmp);
+        Player playerTwo = new Player(scanner.next());
         GuessNumber guessGame = new GuessNumber(playerOne, playerTwo);
 
         String userAnswer = "yes";
-        boolean isRepeat = false;
 
         do {
-            if(isRepeat) {
-                System.out.format("%nВы выбрали продолжить игру. Игроки останутся без изменений.%n");
-                System.out.println("первый игрок: " + playerOne.getName());
-                System.out.println("первый игрок: " + playerTwo.getName());
-            }
-
             guessGame.play();
             do {
                 System.out.format("%nХотите продолжить игру? [yes/no]: %n");
                 userAnswer = scanner.next();
                 if(userAnswer.equals("yes")) {
-                    isRepeat = true;
                 }
             } while(!userAnswer.equals("no") && !userAnswer.equals("yes"));
         } while(!userAnswer.equals("no"));
