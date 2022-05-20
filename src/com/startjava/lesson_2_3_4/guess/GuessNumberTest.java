0c90aec3be98755529dd5f4b72165ba4dc7ef537
amarkovkin <markovkin88@gmail.com>
2022-05-20 20:12:57 +0300

    36. Меняет пакеты для старых и новых файлов. Дополняет gitignore

---
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -0,0 +1,28 @@
+package com.startjava.lesson_2_3_4.guess;
+
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.format("%nДобро пожаловать в игру - угадай число%n");
+        System.out.println("Введите имя первого игрока: ");
+        Player playerOne = new Player(scanner.next());
+        
+        System.out.println("Введите имя второго игрока: ");
+        Player playerTwo = new Player(scanner.next());
+        GuessNumber guessGame = new GuessNumber(playerOne, playerTwo);
+
+        String userAnswer = "yes";
+
+        do {
+            guessGame.play();
+            do {
+                System.out.format("%nХотите продолжить игру? [yes/no]: %n");
+                userAnswer = scanner.next();
+                if(userAnswer.equals("yes")) {
+                }
+            } while(!userAnswer.equals("no") && !userAnswer.equals("yes"));
+        } while(!userAnswer.equals("no"));
+    }
+}
