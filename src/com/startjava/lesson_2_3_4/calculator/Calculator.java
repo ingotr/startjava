1c09c9dd2a820aad65f739d45d0c23693dfa83d4
Markovkin Aleksandr <markovkin88@gmail.com>
2022-05-23 13:02:59 +0300

    41. Добавляет повторный ввод после ввода неверных данных. Переносит интерфейсный текст в константы

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,43 +1,48 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-
-    public static int calculate(String mathExpression) {
+    public static final String INVALID_OPERATOR = "Неизвестный оператор. Введите выражение повторно%n%n";
+    public static final String INVALID_FIRST_NUMBER = "Первое число - не целое. Введите выражение повторно%n%n";
+    public static final String INVALID_SECOND_NUMBER = "Второе число - не целое. Введите выражение повторно%n%n";
+    public static final String INVALID_FORMAT_NUMBER = "Неверный формат чисел. Введите выражение повторно";
+    public static Integer calculate(String mathExpression) {
         String[] partsExpression = mathExpression.split(" ");
-        int a = 0;
+        int a;
         try {
             a = Integer.parseInt(partsExpression[0]);
         } catch (Exception e) {
-            System.out.println("Первое число - не целое. Введите выражение повторно");
+            System.out.format(INVALID_FIRST_NUMBER);
+            return null;
         }
-        char sign = 0;
+        char sign;
         try {
             sign = partsExpression[1].charAt(0);
         } catch (Exception e) {
-            System.out.println("Неизвестный оператор. Введите выражение повторно");
+            System.out.format(INVALID_OPERATOR);
+            return null;
         }
-        int b = 0;
+        int b;
         try {
             b = Integer.parseInt(partsExpression[2]);
         } catch (Exception e) {
-            System.out.println("Второе число - не целое. Введите выражение повторно");
+            System.out.format(INVALID_SECOND_NUMBER);
+            return null;
         }
         if(a > 0 && b > 0) {
             return switch(sign) {
-                case '+' : yield a + b;
-                case '-' : yield a - b;
-                case '*' : yield a * b;
-                case '/' : yield a / b;
-                case '^' : yield (int) Math.pow(a, b);
-                case '%' : yield a % b;
-                default : {
-                    System.out.println("Неизвестный оператор. Введите выражение повторно");
-                    System.out.println("Доступные операторы: +, -, *, /, ^, %");
-                    yield -1;
+                case '+' -> a + b;
+                case '-' -> a - b;
+                case '*' -> a * b;
+                case '/' -> a / b;
+                case '^' -> (int) Math.pow(a, b);
+                case '%' -> a % b;
+                default -> {
+                    System.out.format(INVALID_OPERATOR);
+                    yield null;
                 }
             };
         }
-        System.out.println("Неверный формат чисел. Введите выражение повторно");
-        return -1;
+        System.out.format(INVALID_FORMAT_NUMBER);
+        return null;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,21 +3,25 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
+    public static final String INTRO_TEXT = """
+            Введите математическое выражение в формате
+            Формат чисел: целые положительные%nДоступные операторы: +, -, *, /, ^, %
+            (первое_число пробел знак_операции пробел второе_число):""";
+    public static final String CONTINUE_TEXT = "Хотите продолжить вычисления? [yes/no]: ";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userAnswer = "yes";
+        String userAnswer;
         
         do {
-            System.out.format("Введите математическое выражение в формате " +
-                    "%nФормат чисел: целые положительные" +
-                    "%nДоступные операторы: +, -, *, /, ^, %%n" +
-                    "%n(первое_число пробел знак_операции пробел второе_число): %n");
-            String mathExpression = scanner.nextLine();
-
-            System.out.println("результат: " + Calculator.calculate(mathExpression));
-            
+            Integer result;
             do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]: ");
+                System.out.println(INTRO_TEXT);
+                result = Calculator.calculate(scanner.nextLine());
+            } while(result == null);
+            System.out.format("результат: %d%n", result);
+            do {
+                System.out.println(CONTINUE_TEXT);
                 userAnswer = scanner.nextLine();
             } while(!userAnswer.equals("no") && !userAnswer.equals("yes"));
         } while(!userAnswer.equals("no"));
