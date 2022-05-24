80a19445de57f467a63fd199f250908a11b30e27
Markovkin Aleksandr <markovkin88@gmail.com>
2022-05-24 10:26:55 +0300

    43. Добавляет массив для хранения введенных игроком чисел

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,9 +1,15 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
+
 public class Player {
 
+    public static final String NUMBER_WARNING = "Введите цело число в интервале от 1 до 100(включительно)";
+
     private String name;
-    private int number;
+    private int[] number = new int[10];
+
+    private int tryNumber = 0;
 
     public Player(String name) {
         this.name = name;
@@ -14,14 +20,35 @@ public class Player {
     }
 
     public int getNumber() {
-        return number;
+        setTryNumber();
+        return number[this.tryNumber - 1];
     }
 
     public void setNumber(int number) {
         if(number <=0 || number > 100) {
-            System.out.println("Введите цело число в интервале от 1 до 100(включительно)");
+            System.out.println(NUMBER_WARNING);
         } else {
-            this.number = number;
+            this.number[this.tryNumber] = number;
         }
+    }
+
+    public int getTryNumber() {
+        return this.tryNumber;
+    }
+
+    public void setTryNumber() {
+        ++this.tryNumber;
+    }
+
+    public void resetTryNumber(int num) {
+        this.tryNumber = num;
+    }
+
+    public void resetNumber() {
+        Arrays.fill(this.number, 0, this.tryNumber, 0);
+    }
+
+    public int[] getCopyNumber() {
+        return Arrays.copyOf(number, this.tryNumber);
     }
 }
