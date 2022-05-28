348085eaee0b70ca43ae492c2718e80dd21399a0
Markovkin Aleksandr <markovkin88@gmail.com>
2022-05-28 12:33:47 +0300

    53. Создает нескольких игроков в массиве

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -6,12 +6,8 @@ public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.format("%nДобро пожаловать в игру - угадай число%n");
-        System.out.format("Введите имя %s игрока: %n", "первого");
-        Player playerOne = new Player(scanner.next());
-        
-        System.out.format("Введите имя %s игрока: %n", "второго");
-        Player playerTwo = new Player(scanner.next());
-        GuessNumber guessGame = new GuessNumber(playerOne, playerTwo);
+
+        GuessNumber guessGame = new GuessNumber(createPlayers(scanner));
         String userAnswer = "yes";
 
         do {
@@ -21,5 +17,16 @@ public class GuessNumberTest {
                 userAnswer = scanner.next();
             } while(!userAnswer.equals("no") && !userAnswer.equals("yes"));
         } while(!userAnswer.equals("no"));
+    }
+
+    public static Player[] createPlayers(Scanner scanner) {
+        Player[] players = new Player[3];
+        int i = 0;
+        do{
+            System.out.format("Введите имя %s игрока: %n", i + 1);
+            players[i] = new Player(scanner.next());
+            i++;
+        } while(i < players.length);
+        return players;
     }
 }
